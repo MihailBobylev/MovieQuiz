b4d3bfa2fe649d2d63597c7a25075d0700ef9369
Bobylev Mikhail <bobylev.mikhail@qrme.su>
2025-04-03 17:00:38 +0300

    Реализация архитектуры MVP

--- MovieQuiz/Presentation/MovieQuizPresenter.swift
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -8,9 +8,74 @@
 import UIKit
 
 final class MovieQuizPresenter {
-    let questionsAmount: Int = 10
-    private var currentQuestionIndex: Int = 0
+    private let statisticService: StatisticServiceProtocol
+    private var questionFactory: QuestionFactoryProtocol?
+    private weak var viewController: MovieQuizViewControllerProtocol?
     
+    private let questionsAmount: Int = 10
+    private var currentQuestionIndex: Int = 0
+    private var currentQuestion: QuizQuestion?
+    private var correctAnswers: Int = 0
+    
+    init(viewController: MovieQuizViewControllerProtocol, statisticService: StatisticServiceProtocol = StatisticService()) {
+        self.viewController = viewController
+        self.statisticService = statisticService
+        
+        questionFactory = QuestionFactory(moviesLoader: MoviesLoader(), delegate: self)
+        questionFactory?.loadData()
+        viewController.showLoadingIndicator()
+    }
+    
+    func isLastQuestion() -> Bool {
+        currentQuestionIndex == questionsAmount - 1
+    }
+    
+    func restartGame() {
+        correctAnswers = 0
+        currentQuestionIndex = 0
+        questionFactory?.requestNextQuestion()
+    }
+    
+    func switchToNextQuestion() {
+        currentQuestionIndex += 1
+    }
+    
+    func yesButtonClicked() {
+        handleAnswer(givenAnswer: true)
+    }
+    
+    func noButtonClicked() {
+        handleAnswer(givenAnswer: false)
+    }
+}
+
+extension MovieQuizPresenter: QuestionFactoryDelegate {
+    func didLoadDataFromServer() {
+        viewController?.hideLoadingIndicator()
+        questionFactory?.requestNextQuestion()
+    }
+    
+    func didFailToLoadData(with error: Error) {
+        let message = error.localizedDescription
+        viewController?.showNetworkError(message: message)
+    }
+    
+    func didReceiveNextQuestion(question: QuizQuestion?) {
+        viewController?.hideLoadingIndicator()
+        guard let question else {
+            return
+        }
+
+        currentQuestion = question
+        let viewModel = convert(model: question)
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.viewController?.show(quiz: viewModel)
+        }
+    }
+}
+
+private extension MovieQuizPresenter {
     func convert(model: QuizQuestion) -> QuizStepViewModel {
         let questionStep = QuizStepViewModel(
             image: UIImage(data: model.image) ?? UIImage(),
@@ -19,15 +84,51 @@ final class MovieQuizPresenter {
         return questionStep
     }
     
-    func isLastQuestion() -> Bool {
-        currentQuestionIndex == questionsAmount - 1
+    func didAnswer(isCorrectAnswer: Bool) {
+        if isCorrectAnswer {
+            correctAnswers += 1
+        }
     }
     
-    func resetQuestionIndex() {
-        currentQuestionIndex = 0
+    func handleAnswer(givenAnswer: Bool) {
+        viewController?.disableButtons()
+        
+        guard let currentQuestion else { return }
+        proceedWithAnswer(isCorrect: currentQuestion.correctAnswer == givenAnswer)
     }
     
-    func switchToNextQuestion() {
-        currentQuestionIndex += 1
+    func proceedWithAnswer(isCorrect: Bool) {
+        didAnswer(isCorrectAnswer: isCorrect)
+        viewController?.highlightImageBorder(isCorrectAnswer: isCorrect)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            guard let self else { return }
+            proceedToNextQuestionOrResults()
+        }
+    }
+    
+    func proceedToNextQuestionOrResults() {
+        viewController?.clearImageBorder()
+        if isLastQuestion() {
+            statisticService.store(correct: correctAnswers, total: questionsAmount)
+            
+            let bestGame = statisticService.bestGame
+            let text = """
+            Ваш результат: \(correctAnswers)/\(questionsAmount)
+            Количество сыгранных квизов: \(statisticService.gamesCount)
+            Рекорд: \(bestGame.correct)/\(bestGame.total) (\(bestGame.date.dateTimeString))
+            Средняя точность: \(String(format: "%.2f", statisticService.totalAccuracy))%
+            """
+            
+            let viewModel = QuizResultsViewModel(
+                title: "Этот раунд окончен!",
+                text: text,
+                buttonText: "Сыграть ещё раз")
+            viewController?.show(quiz: viewModel)
+        } else {
+            switchToNextQuestion()
+            viewController?.showLoadingIndicator()
+            questionFactory?.requestNextQuestion()
+        }
     }
 }
--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -8,53 +8,10 @@
 import Foundation
 
 final class QuestionFactory: QuestionFactoryProtocol {
-//MARK: В уроке была просьба это закомменировать, а не удалять
-//    private let questions: [QuizQuestion] = [
-//            QuizQuestion(
-//                image: "The Godfather",
-//                text: "Рейтинг этого фильма больше чем 6?",
-//                correctAnswer: true),
-//            QuizQuestion(
-//                image: "The Dark Knight",
-//                text: "Рейтинг этого фильма больше чем 6?",
-//                correctAnswer: true),
-//            QuizQuestion(
-//                image: "Kill Bill",
-//                text: "Рейтинг этого фильма больше чем 6?",
-//                correctAnswer: true),
-//            QuizQuestion(
-//                image: "The Avengers",
-//                text: "Рейтинг этого фильма больше чем 6?",
-//                correctAnswer: true),
-//            QuizQuestion(
-//                image: "Deadpool",
-//                text: "Рейтинг этого фильма больше чем 6?",
-//                correctAnswer: true),
-//            QuizQuestion(
-//                image: "The Green Knight",
-//                text: "Рейтинг этого фильма больше чем 6?",
-//                correctAnswer: true),
-//            QuizQuestion(
-//                image: "Old",
-//                text: "Рейтинг этого фильма больше чем 6?",
-//                correctAnswer: false),
-//            QuizQuestion(
-//                image: "The Ice Age Adventures of Buck Wild",
-//                text: "Рейтинг этого фильма больше чем 6?",
-//                correctAnswer: false),
-//            QuizQuestion(
-//                image: "Tesla",
-//                text: "Рейтинг этого фильма больше чем 6?",
-//                correctAnswer: false),
-//            QuizQuestion(
-//                image: "Vivarium",
-//                text: "Рейтинг этого фильма больше чем 6?",
-//                correctAnswer: false)
-//        ]
-    
     private let moviesLoader: MoviesLoading
     private var movies: [MostPopularMovie] = []
     private weak var delegate: QuestionFactoryDelegate?
+    private let questionRating = 9
     
     init(moviesLoader: MoviesLoading, delegate: QuestionFactoryDelegate?) {
         self.moviesLoader = moviesLoader
@@ -94,8 +51,8 @@ final class QuestionFactory: QuestionFactoryProtocol {
             
             let rating = Float(movie.rating) ?? 0
             
-            let text = "Рейтинг этого фильма больше чем 7?"
-            let correctAnswer = rating > 7
+            let text = "Рейтинг этого фильма больше чем \(questionRating)?"
+            let correctAnswer = rating > Float(questionRating)
             
             let question = QuizQuestion(image: imageData,
                                         text: text,
